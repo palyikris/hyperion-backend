47f45f195cd033e31e5c5ba945dcc7e3fab0783d
Kribbany <palyi.kristof@gmail.com>
2026-02-15 17:34:20 +0100

    Update CORS middleware to include localhost in allowed origins

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:5173")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[FRONTEND_URL, "http://127.0.0.1:5173"],
+    allow_origins=[FRONTEND_URL, "http://127.0.0.1:5173", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
