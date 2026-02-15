3c5796155ae71963885fe29c9f60d2a6defb6ee6
Kribbany <palyi.kristof@gmail.com>
2026-02-15 18:02:39 +0100

    Restrict allowed headers in CORS middleware to specific values

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ app.add_middleware(
     allow_origins=[FRONTEND_URL, "http://127.0.0.1:5173", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["Authorization", "Content-Type"],
 )
 
 # Public Routes
