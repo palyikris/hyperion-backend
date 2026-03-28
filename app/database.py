8fc73254f34c7844a015b7f4a943c40210e1cc28
kribbany <palyi.kristof@gmail.com>
2026-03-28 16:57:05 +0100

    fix: Disable SQLAlchemy echo and adjust connection pool settings

--- app/database.py
+++ app/database.py
@@ -11,10 +11,10 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(
     DATABASE_URL or "",
-    echo=True,
+    echo=False,
     pool_pre_ping=True,  # verify connections are alive before using them
-    pool_size=10,
-    max_overflow=20,
+    pool_size=20,
+    max_overflow=10,  # allow up to 10 additional connections beyond the pool_size when needed
 )
 AsyncSessionLocal = async_sessionmaker(
     bind=engine, class_=AsyncSession, expire_on_commit=False
